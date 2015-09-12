815f13340714d1db41cee6e67ff54b9495f62c42
Nick Chidsey <nchidsey@users.noreply.github.com>
2015-09-12 11:43:53 -0400

    added an additional set of tests beyond the json, including many exceptions

--- test/test_schedule.py
+++ test/test_schedule.py
@@ -11,7 +11,7 @@ import datetime
 
 import pytest
 
-from schyntax import Schedule
+from schyntax import Schedule, SchyntaxParseException, InvalidScheduleException
 
 
 def _gather_cases():
@@ -42,4 +42,76 @@ def test_json_data(fmt, date, prev, next):
     schedule = Schedule(fmt)
     assert next == schedule.next(date)
     assert prev == schedule.previous(date)
+
+
+@pytest.mark.parametrize('fmt', [
+    # empty
+    "",
+    " ",
+    "{}",
+])
+def test_invalid_schedule_exception(fmt):
+    with pytest.raises(InvalidScheduleException):
+        Schedule(fmt)
+
+
+@pytest.mark.parametrize('fmt', [
+    # bad expression name (at least)
+    "foo",
+    "foo()",
+    "foo(*)",
+    
+    # bad parens
+    "(",
+    ")",
+    "()",
+    "{",
+    "}",
+    
+    # unexpected symbols
+    "/",
+    ":",
+    '"',
+    
+    # bare attribs
+    "1",
+    "*",
+    "monday",
+    
+    # incomplete expressions
+    "minute",
+    "minute(",
+    "minute()",
+    "minute(5",
+    "minute(5..",
+    "minute(5..)",       # not sure about this one
+    "minute(5%",
+    "minute(5%)",
+    "minute(!",
+    "minute(!%",
+    "minute(!%)",
+    
+    
+    # wrong attribute types
+# TODO - this test fails (meaning it runs without exception). not sure how reference implementation handles it.
+#    "minute(monday)",
+
+    "minute(4/1)",
+    
+    # bad attribute values
+    "minute(foo)",
+    
+    # attribute out of legal range
+    "minute(-1)",
+    "minute(60)",
+    "minute(999999999999999999999999999999999999999)",
+    "dayofweek(8)",
+    
+    # otherwise bogus
+    "minute 5",
+    
+])
+def test_parse_exception(fmt):
+    with pytest.raises(SchyntaxParseException):
+        Schedule(fmt)
 
